7dc4ee1b798ae5ad6fe27560e5b86083a7556b36
Voskanyan20 <arpman9900@gmail.com>
2023-06-16 16:50:42 +0400

    upd

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const { checkPermission } = require("./src/rbac");
+// const { checkPermission } = require("./src/rbac");
 
-checkPermission('John Doe', 'Home reed');
+const axios = require("axios");
+
+const checkPermission = (userName, permissionTitle, uuid) => {
+    axios.post("http://localhost:8080/private/checkUserPermissions/", { userName: userName, permissionTitle: permissionTitle }, { headers: { "Authorization": uuid } }).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+// module.exports = {checkPermission}
